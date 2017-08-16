418420fb83b5c6379bdb6882963e3f81f3cfd0ef
Adrian Schaedle <adrian@schaedle.me>
2017-08-16 06:40:50 -0400

    Remove dependencies on window and mouse
    We can just listen for events on the svg element itself.

--- Main.elm
+++ Main.elm
@@ -7,14 +7,12 @@ import Html exposing (Html)
 import Html.Attributes
 import Html.Events
 import Json.Decode
-import Mouse
 import Random
 import Svg exposing (Svg)
 import Svg.Attributes as Attributes
 import Svg.Path
 import Svg.Events
 import Task
-import Window
 
 
 main : Program Never Model Msg
@@ -39,8 +37,6 @@ type Model
 type alias Image =
     { colorscheme : Colorscheme
     , progress : Float
-    , iterations : Int
-    , angle : Int
     , canvas : { width : Int }
     , system : System
     , editor : Bool
@@ -49,6 +45,8 @@ type alias Image =
 
 type alias System =
     { start : String
+    , angle : Int
+    , iterations : Int
     , rules : Dict String String
     }
 
@@ -67,21 +65,68 @@ type alias Colorscheme =
 
 
 type Msg
-    = Generate
+    = Randomize
     | Build Color
     | ShowEditor
     | CloseEditor
-    | MoveMouse Mouse.Position
-    | Resize Window.Size
+    | ChangeProgress Float
 
 
 init : ( Model, Cmd Msg )
 init =
     let
         loadCommand =
-            Task.succeed Generate |> Task.perform identity
+            Task.succeed Randomize |> Task.perform identity
     in
         ( Loading, loadCommand )
+
+
+systemOne : System
+systemOne =
+    { start = "NSH"
+    , angle = 90
+    , iterations = 5
+    , rules =
+        Dict.fromList
+            [ ( "F", "H-[F[-]]" )
+            , ( "D", "NNHF" )
+            , ( "N", "[]S-HFSHF" )
+            , ( "H", "[-[[N]DS-]]" )
+            , ( "S", "[N-+SHDN]" )
+            ]
+    }
+
+
+systemTwo : System
+systemTwo =
+    { start = "S"
+    , angle = 60
+    , iterations = 5
+    , rules =
+        Dict.fromList
+            [ ( "F", "JS" )
+            , ( "O", "F+F+FF" )
+            , ( "L", "JF++" )
+            , ( "V", "JL+-OSFF" )
+            , ( "J", "OF+" )
+            , ( "S", "[+S]LS+V" )
+            ]
+    }
+
+
+systemThree : System
+systemThree =
+    { start = "ZZ"
+    , angle = 45
+    , iterations = 5
+    , rules =
+        Dict.fromList
+            [ ( "F", "-ZFFFF-FF" )
+            , ( "Z", "PA" )
+            , ( "P", "+AZ" )
+            , ( "A", "F+[ZF]AZ" )
+            ]
+    }
 
 
 
@@ -91,60 +136,32 @@ init =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Generate ->
+        Randomize ->
             ( model, Random.generate Build randomColor )
 
         Build color ->
             let
-                system =
-                    { start = "NSH"
-                    , rules =
-                        Dict.fromList
-                            [ ( "F", "H-[F[-]]" )
-                            , ( "D", "NNHF" )
-                            , ( "N", "[]S-HFSHF" )
-                            , ( "H", "[-[[N]DS-]]" )
-                            , ( "S", "[N-+SHDN]" )
-                            ]
-                    }
-
                 image =
                     { colorscheme = toColorscheme color
                     , progress = 1
-                    , iterations = 4
-                    , angle = 90
-                    , system = system
+                    , system = systemTwo
                     , canvas = { width = 100 }
                     , editor = False
                     }
             in
                 ( Page image, Cmd.none )
 
-        MoveMouse position ->
+        ChangeProgress progress ->
             case model of
                 Loading ->
                     ( model, Cmd.none )
 
                 Page image ->
                     let
-                        progress =
-                            toFloat position.x / toFloat image.canvas.width
-
                         page =
                             Page { image | progress = progress }
-
-                        command =
-                            Task.perform Resize Window.size
                     in
-                        ( page, command )
-
-        Resize { width } ->
-            case model of
-                Loading ->
-                    ( model, Cmd.none )
-
-                Page image ->
-                    ( Page { image | canvas = { width = width } }, Cmd.none )
+                        ( page, Cmd.none )
 
         ShowEditor ->
             case model of
@@ -199,27 +216,31 @@ toColorscheme color =
         Colorscheme background foreground
 
 
-expand : Int -> System -> String
-expand iterations { start, rules } =
-    if iterations < 1 then
-        start
+expand : System -> String
+expand system =
+    if system.iterations < 1 then
+        system.start
     else
         let
             newStart =
-                start
+                system.start
                     |> String.split ""
-                    |> List.filterMap (\char -> Dict.get char rules)
+                    |> List.filterMap (\char -> Dict.get char system.rules)
                     |> String.join ""
         in
-            expand (iterations - 1) { start = newStart, rules = rules }
+            expand
+                { system
+                    | start = newStart
+                    , iterations = system.iterations - 1
+                }
 
 
-toPath : String -> Svg.Path.Subpath
-toPath string =
+toPath : Int -> String -> Svg.Path.Subpath
+toPath angle string =
     Svg.Path.subpath
         (Svg.Path.startAt ( 0, 0 ))
         Svg.Path.open
-        (toPathHelp string { current = ( 0, 0 ), angle = 0, stack = [] })
+        (toPathHelp string { current = ( 0, 0 ), angle = angle, stack = [] })
 
 
 toPathHelp :
@@ -250,10 +271,10 @@ toPathHelp string cursor =
                         Svg.Path.lineTo point :: (toPathHelp rest { cursor | current = point })
 
                 '+' ->
-                    toPathHelp rest { cursor | angle = cursor.angle + 90 }
+                    toPathHelp rest { cursor | angle = cursor.angle + 45 }
 
                 '-' ->
-                    toPathHelp rest { cursor | angle = cursor.angle - 90 }
+                    toPathHelp rest { cursor | angle = cursor.angle - 45 }
 
                 '[' ->
                     let
@@ -288,7 +309,7 @@ toPathHelp string cursor =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Window.resizes Resize
+    Sub.none
 
 
 
@@ -316,7 +337,6 @@ stylesheet =
 
 .Button:hover {
     color: white;
-    background-color: #444;
 }
 """
 
@@ -339,28 +359,33 @@ view model =
                     , controlsView system
                     , Html.node "style" [] [ Html.text stylesheet ]
                     ]
-                , Html.div
-                    [ Html.Attributes.style
-                        [ ( "padding", "50px" )
-                        , ( "max-width", "700px" )
-                        , ( "margin-left", "auto" )
-                        , ( "margin-right", "auto" )
-                        , ( "line-height", "1.8" )
-                        ]
-                    ]
-                    [ Html.h2 [] [ Html.text "What is an L-system?" ]
-                    , Html.p [] [ Html.text "An L-system or Lindenmayer system is a parallel rewriting system and a type of formal grammar. An L-system consists of an alphabet of symbols that can be used to make strings, a collection of production rules that expand each symbol into some larger string of symbols, an initial \"axiom\" string from which to begin construction, and a mechanism for translating the generated strings into geometric structures. L-systems were introduced and developed in 1968 by Aristid Lindenmayer, a Hungarian theoretical biologist and botanist at the University of Utrecht. Lindenmayer used L-systems to describe the behaviour of plant cells and to model the growth processes of plant development. L-systems have also been used to model the morphology of a variety of organisms[1] and can be used to generate self-similar fractals such as iterated function systems." ]
-                    , Html.h2 [] [ Html.text "Origins" ]
-                    , Html.p [] [ Html.text "As a biologist, Lindenmayer worked with yeast and filamentous fungi and studied the growth patterns of various types of algae, such as the cyanobacteria Anabaena catenula. Originally the L-systems were devised to provide a formal description of the development of such simple multicellular organisms, and to illustrate the neighbourhood relationships between plant cells. Later on, this system was extended to describe higher plants and complex branching structures." ]
-                    , Html.h2 [] [ Html.text "L-system structure" ]
-                    , Html.p [] [ Html.text "The recursive nature of the L-system rules leads to self-similarity and thereby, fractal-like forms are easy to describe with an L-system. Plant models and natural-looking organic forms are easy to define, as by increasing the recursion level the form slowly 'grows' and becomes more complex. Lindenmayer systems are also popular in the generation of artificial life." ]
-                    , Html.p [] [ Html.text "L-system grammars are very similar to the semi-Thue grammar (see Chomsky hierarchy). L-systems are now commonly known as parametric L systems, defined as a tuple" ]
-                    ]
+                , prose
                 ]
 
 
+prose : Html msg
+prose =
+    Html.div
+        [ Html.Attributes.style
+            [ ( "padding", "50px" )
+            , ( "max-width", "700px" )
+            , ( "margin-left", "auto" )
+            , ( "margin-right", "auto" )
+            , ( "line-height", "1.8" )
+            ]
+        ]
+        [ Html.h2 [] [ Html.text "What is an L-system?" ]
+        , Html.p [] [ Html.text "An L-system or Lindenmayer system is a parallel rewriting system and a type of formal grammar. An L-system consists of an alphabet of symbols that can be used to make strings, a collection of production rules that expand each symbol into some larger string of symbols, an initial \"axiom\" string from which to begin construction, and a mechanism for translating the generated strings into geometric structures. L-systems were introduced and developed in 1968 by Aristid Lindenmayer, a Hungarian theoretical biologist and botanist at the University of Utrecht. Lindenmayer used L-systems to describe the behaviour of plant cells and to model the growth processes of plant development. L-systems have also been used to model the morphology of a variety of organisms[1] and can be used to generate self-similar fractals such as iterated function systems." ]
+        , Html.h2 [] [ Html.text "Origins" ]
+        , Html.p [] [ Html.text "As a biologist, Lindenmayer worked with yeast and filamentous fungi and studied the growth patterns of various types of algae, such as the cyanobacteria Anabaena catenula. Originally the L-systems were devised to provide a formal description of the development of such simple multicellular organisms, and to illustrate the neighbourhood relationships between plant cells. Later on, this system was extended to describe higher plants and complex branching structures." ]
+        , Html.h2 [] [ Html.text "L-system structure" ]
+        , Html.p [] [ Html.text "The recursive nature of the L-system rules leads to self-similarity and thereby, fractal-like forms are easy to describe with an L-system. Plant models and natural-looking organic forms are easy to define, as by increasing the recursion level the form slowly 'grows' and becomes more complex. Lindenmayer systems are also popular in the generation of artificial life." ]
+        , Html.p [] [ Html.text "L-system grammars are very similar to the semi-Thue grammar (see Chomsky hierarchy). L-systems are now commonly known as parametric L systems, defined as a tuple" ]
+        ]
+
+
 controlsView : Image -> Html Msg
-controlsView { iterations, angle, system, editor } =
+controlsView { system, editor } =
     Html.div
         [ Html.Attributes.style
             [ ( "background-color", colorToHex (Color.grayscale 0.9) )
@@ -396,13 +421,13 @@ controlsView { iterations, angle, system, editor } =
                 (rulesView system.rules)
              , Html.div
                 [ Html.Attributes.style [ ( "margin-bottom", "40px" ) ] ]
-                [ Html.text <| "angle: " ++ (angle |> toString |> String.left 5) ++ " degrees" ]
+                [ Html.text <| "angle: " ++ (system.angle |> toString |> String.left 5) ++ " degrees" ]
              , Html.div
                 [ Html.Attributes.style [ ( "text-wrap", "break-word" ), ( "margin-bottom", "40px" ) ] ]
                 [ Html.text "valid characters in the rules include [ (add a new level on the stack), ] (pop a level off the stack), + (turn clockwise by given angle), - (counterclockwise), or another rule. The rules above will get expanded into:" ]
              , Html.div
                 [ Html.Attributes.style [ ( "text-wrap", "break-word" ) ] ]
-                [ Html.text <| expand iterations system ]
+                [ Html.text <| expand system ]
              ]
             )
          else
@@ -486,13 +511,13 @@ points =
 
 
 systemView : Image -> Svg Msg
-systemView { colorscheme, progress, system, iterations } =
+systemView { colorscheme, progress, system } =
     let
         styles =
             "background-color:" ++ colorToHex colorscheme.background
 
         path =
-            expand iterations system |> toPath
+            expand system |> toPath system.angle
     in
         Svg.svg
             [ Attributes.style styles
@@ -500,21 +525,21 @@ systemView { colorscheme, progress, system, iterations } =
             , Attributes.height "100%"
             , Attributes.preserveAspectRatio "xMidYMid meet"
             , Attributes.viewBox <| viewboxFromPath path
-            , Svg.Events.on "mousemove" (Json.Decode.map MoveMouse mousePosition)
+            , Svg.Events.on "mousemove" (Json.Decode.map ChangeProgress toProgress)
             ]
             [ pathView path colorscheme.foreground ]
 
 
 viewboxFromPath : Svg.Path.Subpath -> String
 viewboxFromPath _ =
-    "-100 -200 300 400"
+    "-200 -200 1800 500"
 
 
-mousePosition : Json.Decode.Decoder Mouse.Position
-mousePosition =
-    Json.Decode.map2 Mouse.Position
-        (Json.Decode.field "clientX" Json.Decode.int)
-        (Json.Decode.field "clientY" Json.Decode.int)
+toProgress : Json.Decode.Decoder Float
+toProgress =
+    Json.Decode.map2 (/)
+        (Json.Decode.field "clientX" Json.Decode.float)
+        (Json.Decode.at [ "target", "clientWidth" ] Json.Decode.float)
 
 
 clamp : comparable -> comparable -> comparable -> comparable
